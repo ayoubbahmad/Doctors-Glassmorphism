3470df57e968dee20565a1184006985ddedf3069
ayoubbahmad <ayoubbahmad@gmail.com>
2021-01-19 23:42:49 +0100

    change babel configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          src: './src',
+          components: './src/components',
+          containers: './src/containers',
+          localization: './src/localization',
+          screens: './src/screens',
+          utils: './src/utils',
+          underscore: 'lodash',
+        },
+      },
+    ],
+  ],
 };
